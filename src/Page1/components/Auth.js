a414c26f55bf2c1a8d07bfe49589d736c58e6570
Philip Harmash <infilya@ukr.net>
2023-05-18 13:40:34 +0300

    add aunt

--- src/Page1/components/Auth.js
+++ src/Page1/components/Auth.js
@@ -6,7 +6,7 @@ export function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-    console.log(auth?.currentUser?.email);
+    // console.log(auth?.currentUser?.email);
 
   async function signIn() {
     try {
