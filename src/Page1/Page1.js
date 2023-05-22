73113dd9512bac070e7273e1a51042c832bea1ad
Philip Harmash <infilya@ukr.net>
2023-05-22 18:00:14 +0300

    remove mail

--- src/Page1/Page1.js
+++ src/Page1/Page1.js
@@ -6,16 +6,13 @@ import "../css/service.css";
 import "../css/ratings.css";
 import { NavLink } from "react-router-dom";
 import { FirstPage } from "./components/FirstPage";
-import { auth } from "../config/firebase";
 
 export default function Page1() {
-  const registratedUser = auth?.currentUser?.uid !== undefined;
-
   return (
     <>
       <FirstPage />
       <div className="showRatings">
-        {registratedUser && <NavLink to={"/Ratings"}>Ratings</NavLink>}
+        <NavLink to={"/Ratings"}>Ratings</NavLink>
         <NavLink to={"/Distribution"}>Distribution</NavLink>
       </div>
     </>
--- src/Page1/components/Auth.js
+++ src/Page1/components/Auth.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { auth, googleProvider } from "../../config/firebase";
-import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { setShowEmailField } from "../../states/reducers/showEmailFieldReducer";
 import { SetDate } from "./SetDate";
@@ -9,8 +9,6 @@ export function Auth() {
   const dispatch = useDispatch();
   const rivalClub = useSelector((state) => state.rivalClub);
   const myClub = useSelector((state) => state.myClub);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const showEmailField = useSelector((state) => state.showEmailField);
   const registratedUser = auth?.currentUser?.uid !== undefined;
   const userPhoto = auth?.currentUser?.photoURL;
@@ -18,13 +16,6 @@ export function Auth() {
   useEffect(() => {
     dispatch(setShowEmailField(registratedUser));
   }, [dispatch, registratedUser]);
-  async function signIn() {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      console.error(err);
-    }
-  }
   async function signInWithGoogle() {
     try {
       await signInWithPopup(auth, googleProvider);
@@ -58,7 +49,7 @@ export function Auth() {
             <img src={userPhoto} alt="" style={{ marginRight: 20, height: 60, borderRadius: 50 }} />
             <h2>{userName}</h2>
           </div>
-          <div style={{fontSize:40 , fontWeight:600}}>
+          <div style={{ fontSize: 40, fontWeight: 600 }}>
             {rivalClub.name} vs {myClub.name}
           </div>
         </>
@@ -66,18 +57,7 @@ export function Auth() {
         <div style={{ height: 67.8, width: 350 }}></div>
       )}
       <div className="registrPanel">
-        {!showEmailField && (
-          <>
-            <input placeholder="Email..." type="text" onChange={(e) => setEmail(e.target.value)} />
-            <input
-              placeholder="Password..."
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={signIn}>Sign in</button>
-            <button onClick={signInWithGoogle}>Sign in with Google</button>
-          </>
-        )}
+        {!showEmailField && <button onClick={signInWithGoogle}>Sign in with Google</button>}
         {showEmailField && (
           <div style={{ width: 350 }}>
             <SetDate />
