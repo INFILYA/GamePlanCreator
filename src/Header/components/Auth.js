b3e20a7cca78a88dfbb4f5fcb150dc53192cf02f
Philip Harmash <infilya@ukr.net>
2023-05-29 12:10:39 +0300

    fixed

--- src/Header/components/Auth.js
+++ src/Header/components/Auth.js
@@ -1,7 +1,6 @@
-import { auth, googleProvider } from "../../config/firebase";
+import { auth, facebookProvider, googleProvider } from "../../config/firebase";
 import {
   signInWithPopup,
-  FacebookAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
@@ -12,51 +11,43 @@ export function Auth() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showEmailPanel, setShowEmailPanel] = useState(false);
   const [showSignInPanel, setShowSignInPanel] = useState(false);
 
   async function signInWithGoogle() {
     try {
       await signInWithPopup(auth, googleProvider);
+      navigate("/");
     } catch (err) {
       console.error(err);
-    } finally {
-      navigate("/");
     }
   }
   async function signInWithFaceBook() {
     try {
-      const provider = new FacebookAuthProvider();
-      await signInWithPopup(auth, provider);
+      await signInWithPopup(auth, facebookProvider);
+      navigate("/");
     } catch (err) {
       console.error(err);
-    } finally {
-      navigate("/");
+      alert("Sorry , account with this name already existed. Please , try again");
     }
   }
   async function signInWithEmail(e) {
     e.preventDefault();
     try {
       await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/");
     } catch (err) {
       console.error(err);
       alert("Sorry , account with this name already existed. Please , try again");
-    } finally {
-      setShowEmailPanel(false);
-      setShowSignInPanel(!showSignInPanel);
-      navigate("/");
     }
   }
   async function logInWithEmail(e) {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
     } catch (err) {
       console.error(err);
       alert("Sorry , we cant find your account. Please , create a new one");
-    } finally {
-      setShowEmailPanel(!showEmailPanel);
-      navigate("/");
     }
   }
   return (
