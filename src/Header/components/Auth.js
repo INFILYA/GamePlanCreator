f84b8c9aa1db18aaea87239a7f6f26a64b7136e2
Philip Harmash <infilya@ukr.net>
2023-06-05 10:40:09 +0300

    !

--- src/Header/components/Auth.js
+++ src/Header/components/Auth.js
@@ -1,19 +1,71 @@
 import { auth, facebookProvider, googleProvider } from "../../config/firebase";
 import {
   signInWithPopup,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  // sendSignInLinkToEmail,
+  // createUserWithEmailAndPassword,
+  // signInWithEmailAndPassword,
+  sendSignInLinkToEmail,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
 } from "firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router";
 
 export function Auth() {
   const navigate = useNavigate();
+  const [isRegistratedUser] = useAuthState(auth);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showSignInPanel, setShowSignInPanel] = useState(false);
+  // const [password, setPassword] = useState("");
+  const [loginLoading, setLoginLoading] = useState(false);
+  const [loginError, setLoginError] = useState("");
+  // const [showSignInPanel, setShowSignInPanel] = useState(false);
 
+  useEffect(() => {
+    if (isRegistratedUser) {
+      // navigate("/");
+    } else {
+      if (isSignInWithEmailLink(auth, window.location.href)) {
+        let email = localStorage.getItem("userEmail");
+        console.log(email);
+        if (!email) {
+          email = window.prompt("Please provide your email");
+        }
+        signInWithEmailLink(auth, localStorage.getItem("userEmail"), window.location.href);
+        localStorage.removeItem("userEmail");
+      }
+    }
+  }, [isRegistratedUser, navigate]);
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      setLoginLoading(true);
+      await sendSignInLinkToEmail(auth, email, {
+        url: "https://game-plan-creator.web.app/Auth",
+        handleCodeInApp: true,
+        dynamicLinkDomain: "gameplancreator.page.link",
+      });
+      localStorage.setItem("userEmail", JSON.stringify(email));
+      alert("We have sent you link on email");
+      setLoginLoading(false);
+      setLoginError("");
+    } catch (err) {
+      console.error(err);
+      setLoginLoading(false);
+      setLoginError(err.message);
+      alert("Sorry , account with this name already existed. Please , try again");
+    }
+  }
+  // async function logInWithEmail(e) {
+  //   e.preventDefault();
+  //   try {
+  //     await signInWithEmailAndPassword(auth, email, password);
+  //     navigate("/");
+  //   } catch (err) {
+  //     console.error(err);
+  //     alert("Sorry , we cant find your account. Please , create a new one");
+  //   }
+  // }
   async function signInWithGoogle() {
     try {
       await signInWithPopup(auth, googleProvider);
@@ -31,37 +83,9 @@ export function Auth() {
       alert("Sorry , account with this name already existed. Please , try again");
     }
   }
-  async function signInWithEmail(e) {
-    e.preventDefault();
-    try {
-      // await sendSignInLinkToEmail(auth, email, {
-      //   // url: "https://game-plan-creator.web.app/Auth",
-      //   url: "http://localhost:3000",
-      //   handleCodeInApp: true,
-      //   dynamicLinkDomain: "gameplancreator.page.link",
-      // });
-      // localStorage.setItem("userEmail", email);
-      // alert("We have sent you link on email");
-      await createUserWithEmailAndPassword(auth, email, password);
-      navigate("/");
-    } catch (err) {
-      console.error(err);
-      alert("Sorry , account with this name already existed. Please , try again");
-    }
-  }
-  async function logInWithEmail(e) {
-    e.preventDefault();
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate("/");
-    } catch (err) {
-      console.error(err);
-      alert("Sorry , we cant find your account. Please , create a new one");
-    }
-  }
   return (
     <>
-      <form className="emailPanel" onSubmit={showSignInPanel ? signInWithEmail : logInWithEmail}>
+      <form className="emailPanel" onSubmit={handleLogin}>
         <div>
           <label>Email:</label>
           <input
@@ -72,39 +96,26 @@ export function Auth() {
             value={email || ""}
           />
           <label>Password:</label>
-          <input
+          {/* <input
             type="password"
             placeholder="Password..."
             required
             onChange={(e) => setPassword(e.target.value)}
-          />
-          {showSignInPanel ? (
-            <>
-              <button type="submit">Sign in</button>
-              <button type="button" onClick={() => setShowSignInPanel(!showSignInPanel)}>
-                Back
-              </button>
-            </>
-          ) : (
-            <>
-              <button type="submit">Log in</button>
-              <button type="button" onClick={() => navigate("/")}>
-                Back
-              </button>
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <button type="button" className="google" onClick={signInWithGoogle}></button>
-                <button type="button" className="facebook" onClick={signInWithFaceBook}></button>
-              </div>
-              <div>Dont have an account?</div>
-              <button
-                type="button"
-                className="createAccount"
-                onClick={() => setShowSignInPanel(!showSignInPanel)}
-              >
-                Create an account
-              </button>
-            </>
-          )}
+          /> */}
+          <button type="submit">{loginLoading ? "Logging you in" : "Log in"}</button>
+          {loginError !== "" && <div>{loginError}</div>}
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button type="button" className="google" onClick={signInWithGoogle}></button>
+            <button type="button" className="facebook" onClick={signInWithFaceBook}></button>
+          </div>
+          <div>Dont have an account?</div>
+          {/* <button
+            type="button"
+            className="createAccount"
+            onClick={() => setShowSignInPanel(!showSignInPanel)}
+          >
+            Create an account
+          </button> */}
         </div>
       </form>
     </>
