fe7048848634d1f503286c7d78801aaaf875227c
Philip Harmash <infilya@ukr.net>
2023-09-14 17:28:33 +0300

    small changes

--- src/Distribution/components/DistrField.js
+++ src/Distribution/components/DistrField.js
@@ -105,7 +105,7 @@ export function DistrField() {
                 </div>
                 <div className="line-wrapper">
                   <div className="input-wrapper">
-                    <span>Zone 5</span>
+                    <input value={"Zone 5"} readOnly />
                   </div>
                   {inputDistributionArr.slice(3, 5).map((input) => (
                     <InputDistribution
