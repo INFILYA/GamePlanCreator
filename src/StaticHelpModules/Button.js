c90c86e99c1ef5b42a3e953e8783642b1a1f4d91
Philip Harmash <infilya@ukr.net>
2024-04-03 18:55:25 -0400

    add

--- src/StaticHelpModules/Button.js
+++ src/StaticHelpModules/Button.js
@@ -8,7 +8,7 @@ export function Button({ onClick, value, style }) {
 export function upgradeAge(player) {
   const age1 = new Date().getTime();
   const age2 = Date.parse(player.age);
-  const newAge = Math.floor((age1 - age2) / (1000 * 60 * 60 * 24 * 30 * 12));
+  const newAge = Math.floor((age1 - age2) / (1000 * 60 * 60 * 24 * 365));
   const newPlayer = { ...player, age: newAge };
   return newPlayer;
 }
